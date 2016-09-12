d76de293a3016bf3b0555863df3b7a37c6e5e7ac
Olya <loli8866@gmail.com>
2016-09-12 17:43:00 +0300

    created project

---
+++ page-guarantees.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Guarantees
+ */
+?>
+
+<?php get_header(); ?>
+
+<!--TOP-BLOCK-->
+<section class="top-block">
+	<?php get_template_part('inc', 'services-info'); ?>
+</section>
+
+<?php get_template_part('inc', 'guarantees'); ?>
+<?php get_template_part('inc', 'order-steps'); ?>
+<?php get_template_part('inc', 'discount'); ?>
+
+<?php get_footer(); ?>
